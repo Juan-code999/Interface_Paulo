560de2b792d69aa1086ae07c14e374d0db200173
Juan <juancharlesasimoes08@gmail.com>
2024-10-08 17:21:41 -0300

    Resposta 1

---
+++ Exemplo2/Passaro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo2
+{
+    using global::Exemplo1;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Exemplo1
+    {
+        public class Passaro : IAnimal
+        {
+            public void SomAnimal()
+            {
+                Console.WriteLine("Pru pru...");
+            }
+
+            public void Brincar()
+            {
+                Console.WriteLine("Brincando de voar...");
+            }
+
+            public void Comer()
+            {
+                Console.WriteLine("Comendo uma semente...");
+            }
+        }
+    }
+
+}
--- Exemplo2/Program.cs
+++ Exemplo2/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Exemplo1;
 using Exemplo2;
+using Exemplo2.Exemplo1;
+
 Cachorro c = new Cachorro();
 Gato g = new Gato();
+Passaro  p = new Passaro();
 
 GerenciadorAnimal gerrenciado = new GerenciadorAnimal();
 gerrenciado.RealizarAcao(g);
